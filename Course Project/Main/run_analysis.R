88d4a5c6432456f62374ddbb77287e3ce4e067c8
devcnn <devcnn@gmail.com>
2014-05-25 09:57:40 +0530

    Files for Course Project

--- Course Project/Main/run_analysis.R
+++ Course Project/Main/run_analysis.R
@@ -16,38 +16,38 @@ dataY_test <- read.table("y_test.txt",sep="\t")
 n <- max(nrow(dataX_train),nrow(dataX_test))
 
 train_values <- dataX_train[,1]
-train_position <- dataY_train[,1]
+train_activity <- dataY_train[,1]
 test_values <- dataX_test[,1]
-test_position <- dataY_test[,1]
+test_activity <- dataY_test[,1]
 
 length(train_values) <- n
-length(train_position) <- n
+length(train_activity) <- n
 length(test_values) <- n
-length(test_position) <- n
+length(test_activity) <- n
 
-combined <- cbind(train_values,train_position,test_values,test_position)
+combined <- cbind(train_values,train_activity,test_values,test_activity)
 
-colnames(combined) <- c("Training_Values","Training_Position","Testing_Values","Testing_Position")
+colnames(combined) <- c("Training_Values","Training_activity","Testing_Values","Testing_activity")
 
-positions <- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+activities <- c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
 
-train_means <- tapply(combined[,"Training_Values"], combined[,"Training_Position"], mean)
-names(train_means) <- positions
+train_means <- tapply(combined[,"Training_Values"], combined[,"Training_activity"], mean)
+names(train_means) <- activities
 
-train_SDs <- tapply(combined[,"Training_Values"], combined[,"Training_Position"], sd)
-names(train_SDs) <- positions
+train_SDs <- tapply(combined[,"Training_Values"], combined[,"Training_activity"], sd)
+names(train_SDs) <- activities
 
-test_means <- tapply(combined[,"Testing_Values"], combined[,"Testing_Position"], mean)
-names(test_means) <- positions
+test_means <- tapply(combined[,"Testing_Values"], combined[,"Testing_activity"], mean)
+names(test_means) <- activities
 
-test_SDs <- tapply(combined[,"Testing_Values"], combined[,"Testing_Position"], sd)
-names(test_SDs) <- positions
+test_SDs <- tapply(combined[,"Testing_Values"], combined[,"Testing_activity"], sd)
+names(test_SDs) <- activities
 
 new_extract <- cbind(train_means,train_SDs,test_means,test_SDs)
 
 colnames(new_extract) <- c("Mean_Training","SD_Training","Mean_Testing","SD_Testing")
 
-rownames(new_extract) <- positions
+rownames(new_extract) <- activities
 
 Extracted_data <- as.data.frame(new_extract)
 
--- Course Project/README.md
+++ Course Project/README.md
@@ -7,14 +7,14 @@ Load the contents of the 'Main' folder into your working directory or make the '
 ##Description of the script:
 *The script reads the input files (X_train,y_train,X_test,y_test), gathers the data into 4 separate data frames. The data from each data frame is read into a numeric vector each. The 4 vectors thus obtained are combined to form a matrix.
 
-*The y input files give the position corresponding to the X values. y_train files gives the position for which the x value is given in training. Likewise, for the test files.
+*The y input files give the activity corresponding to the X values. y_train files gives the activity for which the x value is given in training. Likewise, for the test files.
 
-*Using the y values as factors, 'tapply' function has been applied to X values to calculate means and standard deviations across the 6 positions, namely, Walking, Walking Upstairs, Walking Downstairs, Sitting, Standing and Laying.
+*Using the y values as factors, 'tapply' function has been applied to X values to calculate means and standard deviations across the 6 activities, namely, Walking, Walking Upstairs, Walking Downstairs, Sitting, Standing and Laying.
 
 *Vectors containing the means and standard deviations are combined together in a matrix called 'new_extract' which is later used to build the 'Extracted_data' data frame.
 
 *The Extracted_data data frame is then written in a file called 'Extracts'.
 
-*'Extracts' file contains the means and standard deviations of the Samsung Data across the 6 positions, both in training and testing. 
+*'Extracts' file contains the means and standard deviations of the Samsung Data across the 6 activities, both in training and testing. 
 
 PS: The code that with in the comments in the script is to read the data from the input files when the input files are present in a different directory other than the working directory.
